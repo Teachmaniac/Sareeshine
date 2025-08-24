85a180efdddd1e083fa029c00fbe841d852b514e
Naumit Agarwal <naumitagarwal17@gmail.com>
2025-08-24 18:58:35 +0000

    make the ui\ux design focusing on indian women theme and culture make it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default async function Home() {
       </div>
 
       {products.length > 0 ? (
-        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
